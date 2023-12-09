9f028089d857e0d82c41f83b4cf26727fcac8a7c
Louis-Charles Biron <louilresbiron6@gmail.com>
2023-12-09 09:51:17 -0500

    update

---
+++ birthday_extension/background.js
@@ -0,0 +1,10 @@
+// JS
+// Template Extension by Louis-Charles Biron
+// Please don't use or credit this code as your own.
+//
+
+chrome.runtime.onInstalled.addListener((e)=>{
+    if (e.reason == "install") {
+        
+    }
+})
---
+++ birthday_extension/popup.js
@@ -0,0 +1,25 @@
+// JS
+// Template Extension by Louis-Charles Biron
+// Please don't use or credit this code as your own.
+//
+
+document.addEventListener("DOMContentLoaded", function() {
+
+//Display version
+chrome.management.getSelf((e)=>{document.getElementById("version").textContent="V"+e.versionName})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

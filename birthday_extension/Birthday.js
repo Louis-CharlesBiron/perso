146a85bd6ef99af15596d1eec1316a57eadb364a
Louis-Charles Biron <louilresbiron6@gmail.com>
2023-12-11 11:16:55 -0500

    update

---
+++ birthday_extension/Birthday.js
@@ -0,0 +1,16 @@
+// JS
+// Birthday manager by Louis-Charles Biron
+// Please don't use or credit this code as your own.
+//
+class Birthday {
+    constructor() {
+        this.date
+        this.name
+        this.isDone
+    }
+
+
+    getRemaining() {
+
+    }
+}
--- birthday_extension/popup.js
+++ birthday_extension/popup.js
@@ -1,25 +1,25 @@
 // JS
-// Template Extension by Louis-Charles Biron
+// Birthday manager by Louis-Charles Biron
 // Please don't use or credit this code as your own.
 //
 
-document.addEventListener("DOMContentLoaded", function() {
+const SHORT_MONTHS = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+
+//On load
+function onload() {
+
+let d = new Date(), m = d.getMonth()
 
 //Display version
-chrome.management.getSelf((e)=>{document.getElementById("version").textContent="V"+e.versionName})
+chrome.management.getSelf((e)=>{document.getElementById("version").textContent="V"+e.version})
+
+//Set months' order
+let months_el = document.querySelectorAll(".month")
+for (let i=0;i<12;i++) months_el[(m+i)%12].style.order=i
 
 
 
 
 
+}onload()
 
-
-
-
-
-
-
-
-
-
-});

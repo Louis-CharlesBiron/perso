0db1bf50749f2cda4731521f80be51f141600e9b
Louis-Charles Biron <louirlesbiron6@gmail.com>
2024-05-10 11:51:09 -0400

    Update

--- NoYTads.v1/backdrop/content.js
+++ NoYTads.v1/backdrop/content.js
@@ -1,29 +1,24 @@
 // JS
 // SkipYTads Extension by Louis-Charles Biron
-// Please don't use nor credit this code as your own.
+// I should really rewrite this code
 //
 
 
 function setInstantInterval(callback, speed) {
-    if (typeof callback == "function") {
         callback()
         return setInterval(callback,speed||1000)
-    }
 }
 
 let isInterval = -1
 
 chrome.runtime.onMessage.addListener(function(message) {
-    if(message.type == "tab_updated" && message.content.info.status == "complete") {//
-        //console.log(message.content.e, message.content.info)
-        init()
-    }
+    if(message.type == "tab_updated" && message.content.info.status == "complete") init()
 })
 
 function init() {
     //In video
     if (document.location.href.includes("watch?v")) {
-        if (isInterval == -1) {isInterval = setInstantInterval(main, 10)}
+        if (isInterval == -1) isInterval = setInstantInterval(main, 10)
     } else {
         clearInterval(isInterval)
         isInterval = -1
@@ -34,11 +29,6 @@ function init() {
         document.querySelectorAll("ytd-promoted-sparkles-web-renderer, .ytp-ad-overlay-slot, .ytp-ad-overlay-image > img, #companion, #player-ads").forEach((el)=>{
                 el.remove()
         })
-
-        //kkdocument.querySelectorAll("[class*='ad-skip']").forEach((el)=>{
-        //kk    console.log(el)
-        //kk    el.click()
-        //kk})
 
         //video ads
         if (document.querySelector(".ytp-preview-ad")) {

